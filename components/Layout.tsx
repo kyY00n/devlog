d2f3c0f4f1495c8379f5ec431c717b659a6d03c9
rosie <rosie@daangn.com>
2023-12-25 01:34:12 +0900

    fix: flex, h-screen, grow 적용
    * footer 를 밑으로 고정하기 위해 바로 위의 요소에 grow 적용

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,11 +9,13 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
   return (
     <>
-      <Header />
-      <main className="transition duration-500 bg-white dark:bg-[#111111] text-black dark:text-white">
-        <div className="max-w-screen-md flex flex-col px-10 m-auto">{props.children}</div>
-      </main>
-      <Footer />
+      <div className="flex flex-col h-screen">
+        <Header />
+        <main className="grow transition duration-500 bg-white dark:bg-[#111111] text-black dark:text-white">
+          <div className="max-w-screen-md flex flex-col px-10 m-auto">{props.children}</div>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
