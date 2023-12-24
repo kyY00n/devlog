3a13635312c8985aa55cebb24d1860799fa9281f
rosie <rosie@daangn.com>
2023-12-24 12:35:30 +0900

    feat: 레이아웃

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import Footer from './Footer';
+import Header from './Header';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout(props: LayoutProps) {
+  return (
+    <>
+      <Header />
+      <main className="transition duration-500 bg-white dark:bg-[#111111] text-black dark:text-white">
+        <div className="max-w-screen-md flex flex-col px-10 m-auto">{props.children}</div>
+      </main>
+      <Footer />
+    </>
+  );
+}
