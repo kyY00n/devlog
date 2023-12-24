022d966b9344a8b55f89576b7777adea93782576
rosie <rosie@daangn.com>
2023-12-24 15:15:24 +0900

    feat: 블로그 글 페이지 생성

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,39 @@
+import { allPosts } from '@/.contentlayer/generated';
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from 'next';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+
+const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const MDXComponent = useMDXComponent(post.body.code);
+  return (
+    <>
+      <div className="mt-10 pb-10 border-b-2 mb-10 prose dark:prose-invert">
+        <h1 className="mb-16">{post.title}</h1>
+        <MDXComponent />
+      </div>
+      {/*<Utterances />*/}
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: allPosts.map((p) => ({ params: { slug: p._raw.flattenedPath } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext) => {
+  const post = allPosts.find((p) => p._raw.flattenedPath === params?.slug);
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export default Post;
