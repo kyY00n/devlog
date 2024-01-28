904cc17c027cf4f6ee7a5dc875937f5d35755e30
Kayoung Yoon <dev.kayoung@gmail.com>
2024-01-28 15:34:04 +0900

    fix: footer 고정 (#10)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,11 +5,10 @@ import Header from './Header';
 interface LayoutProps {
   children: ReactNode;
 }
-
 export default function Layout(props: LayoutProps) {
   return (
     <>
-      <div className="flex flex-col h-screen sm:h-full">
+      <div className="flex flex-col h-screen">
         <Header />
         <main className="grow transition duration-500 bg-white dark:bg-[#111111] text-black dark:text-white">
           <div className="max-w-screen-lg flex flex-col px-10 m-auto">{props.children}</div>
