565778bdce93c59237fc2b5a362ef33927a6e007
Rosie <dev.kayoung@gmail.com>
2024-02-17 17:05:05 +0900

    post: mysql 트랜잭션
    remark gfm 이슈 참고
    https://github.com/contentlayerdev/contentlayer/issues/558

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,6 +2,7 @@ import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 import rehypePrettyCode from 'rehype-pretty-code';
 import { Pluggable } from 'unified';
 import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 
 // 원하는 코드 블록 테마가 있다면 찾아서 적용하면 된다.
 const options = {
@@ -34,15 +35,16 @@ export const Note = defineDocumentType(() => ({
     date: { type: 'string', required: true },
     tags: {
       type: 'list',
-      of: {type: 'string'},
-    }
+      of: { type: 'string' },
+    },
   },
-}))
+}));
 
 export default makeSource({
   contentDirPath: 'documents',
   documentTypes: [Post, Note],
   mdx: {
     rehypePlugins: [rehypeSlug, [rehypePrettyCode, options]] as Pluggable[], // https://stackoverflow.com/questions/77344966/next-js13-contentlayer-syntax-highlight-error-issues
+    remarkPlugins: [remarkGfm],
   },
 });
