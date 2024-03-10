a15ed8064c9b7ca3c2dcae74e57f5358a6012231
Rosie <dev.kayoung@gmail.com>
2024-03-10 21:24:48 +0900

    fix: 페이지 설명 수정

--- pages/notes/index.tsx
+++ pages/notes/index.tsx
@@ -17,7 +17,7 @@ export default function Notes({ notes }: InferGetStaticPropsType<typeof getStati
     <>
       <section className="mt-12 mb-10">
         <h1 className="font-bold text-2xl sm:text-4xl font-UhbeeSehyun">Notes</h1>
-        <h3 className="mt-2 font-bold sm:text-xl">다른 자료를 학습한 기록</h3>
+        <h3 className="mt-2 font-bold sm:text-xl font-UhbeeSehyun">일기, TIL, 자료 기록 등</h3>
       </section>
       <NoteList notes={notes} />
     </>
